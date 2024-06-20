be2d8e32dcf6554bfa0e366345302c6f57ea0f62
Jeremiah-Williams <jae4lani17@gmail.com>
2024-06-20 11:11:08 -0400

    dicthw.py

--- dicthw.py
+++ dicthw.py
@@ -14,4 +14,22 @@ del restaurant_menu["Starters"]["Bruschetta"]
 print(restaurant_menu)
 
 # Customer Service Ticket Tracker
+# Tracks customer service tickets, each with a unique ID, customer name, issue description, and status (open/closed).
+# Implement functions to:
+# Open a new service ticket.
+# Update the status of an existing ticket  to "closed".
+# Display all tickets.
+#   (Bonus) filter by status
+# Initialize with some sample tickets and include functionality for additional ticket entry.
 
+service_tickets = {
+    "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
+    "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
+}
+
+service_tickets["Ticket001"]["Status"] = "closed"
+new_service_ticket = {
+    "Ticket003": {"ID#": "Cumflywus2", "Customer":"James Brown", "Issue": "Ticket_Mixup", "Status": input("(open/closed)")},
+                  }
+print(service_tickets)
+print(new_service_ticket)
